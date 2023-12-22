8a6ddffd5b1fd3b3b69ed79eb30fb95d67d63f4a
rootnure <nur.diu.2791@gmail.com>
2023-12-22 13:05:05 +0600

    Login component added and connected in navbar button

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../pages/Shared/NavBar";
 import Footer from "../pages/Shared/Footer";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <NavBar />
       <main className="min-h-[calc(100vh-208px)] pt-16">
         <Outlet />
       </main>
-      <Footer />
+      {pathname.includes("login") ? "" : <Footer />}
     </>
   );
 };
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <p>This is login page</p>
+    </div>
+  );
+};
+
+export default Login;
